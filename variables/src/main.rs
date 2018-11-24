302bc433ea65820c3fea1f73e1da20c88c391826
Sunil Udupi <udpsunil@gmail.com>
2018-11-24 20:44:55 +0530

    Integer Overflow Example

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,4 +1,13 @@
 fn main() {
     let guess: u32 = "42".parse().expect("Not a number!");
     println!("Guess is {}", guess);
+
+    // integer overflow example
+    let mut x: u8 = 0;
+    x = x + 1;
+    while x > 0 {
+        x = x + 1;
+        println!("Value of x is : {}", x);
+    }
+    println!("X is overflown and it's value now is {}", x)
 }
