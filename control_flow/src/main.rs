70a70223fad32138fa636318fe2979809f32abd6
Sunil U <udpsunil@gmail.com>
2018-11-27 11:06:07 +0530

    Added Control Flow

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,3 +1,58 @@
 fn main() {
-    println!("Hello, world!");
+    let number = 3;
+
+    if number < 5 {
+        println!("Condition was true");
+    } else {
+        println!("Condition was false");
+    }
+
+    let other_number = 6;
+
+    if other_number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if other_number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if other_number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3 or 2");
+    }
+
+    // let and if statements
+    let condition = true;
+    let num = if condition { 5 } else { 6 };
+    println!("The value of num is: {}", num);
+
+    // loop
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    let mut numeral = 3;
+
+    while numeral != 0 {
+        println!("{}!", numeral);
+        numeral = numeral - 1;
+    }
+    println!("LIFTOFF!!!");
+    assert_eq!(result, 20);
+
+    // looping through a collection
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is :{}", a[index]);
+        index += 1;
+    }
+    for element in a.iter() {
+        println!("value is: {}", element);
+    }
 }
---
+++ control_flow/target/debug/control_flow.d
@@ -0,0 +1,1 @@
+C:\Users\39896\IdeaProjects\rust_learn\control_flow\target\debug\control_flow.pdb: C:\Users\39896\IdeaProjects\rust_learn\control_flow\src\main.rs
